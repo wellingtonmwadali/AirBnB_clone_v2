de621fb0d36e0526c3263627c5208c6995f829a4
Alex Maina <lexizgichbuoy@gmail.com>
2023-08-23 12:34:05 -0700

    userdb

--- models/user.py
+++ models/user.py
@@ -5,33 +5,28 @@ import models
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+from sqlalchemy import Column
+from sqlalchemy import String
 
 
 class User(BaseModel, Base):
-    """user representaation """
+    """class that represents user
+    Atrributes:
+        __tablename__: name for sql table
+        email: user email address.
+        passwd: user's password.
+        f_name: user's first name
+        l_name: user's last name
+        places: user-pace relationship
+        reviews: user-review"""
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        _password = Column('password',
-                           String(128),
-                           nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
-    else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
+        email = Column(String(128), nullable=False)
+        passwd = Column('password', String(128), nullable=False)
+        f_name = Column(String(128), nullable=True)
+        l_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="delete-orphan")
+        reviews = relationship("Review", backref="user", cascade="delete")
 
     def __init__(self, *args, **kwargs):
         """initializes user class"""
