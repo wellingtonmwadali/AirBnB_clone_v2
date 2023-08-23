08053e067f369bf3b8fc43df6b79729ec85d15bb
Alex Maina <lexizgichbuoy@gmail.com>
2023-08-23 13:07:25 -0700

    dbstorage

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """class amenity that takes 2 arguments:
+    Attributes:
+        name: name
+        place_amenities: place
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
