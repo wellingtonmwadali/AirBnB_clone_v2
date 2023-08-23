bb1c1ed47ea522b603d54345e54b04bede57dec3
Mwadali <wellingtonmwadali@gmail.com>
2023-08-23 12:59:41 -0700

    init file

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,13 @@
 from models.engine.file_storage import FileStorage
 
 
-storage = FileStorage()
+type_storage = os.getenv('HBNB_TYPE_STORAGE')
+
+
+if type_storage == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
