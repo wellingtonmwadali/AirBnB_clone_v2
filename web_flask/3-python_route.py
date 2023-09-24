8de981a7936bcef769cf9a77a03e81ab133fbbc1
Mwadali <wellingtonmwadali@gmail.com>
2023-09-24 06:24:32 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application:
+    listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return string"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns  string"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python followed by text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
