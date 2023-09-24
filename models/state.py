f4832f4e467fa8f7a79f181749a7e12108d904bd
Mwadali <wellingtonmwadali@gmail.com>
2023-09-24 10:15:08 -0700

    task 7 file

--- models/state.py
+++ models/state.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-""" class State"""
+"""class State"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-from models.city import City
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Representation of state class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="delete", backref="state")
+    """ state representation """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128),
+                      nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                              backref="states")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """ Getter  to return City instances"""
+            """fs getter attribute  returns City instances"""
+            values_city = models.storage.all("City").values()
             list_city = []
             for city in values_city:
                 if city.state_id == self.id:
